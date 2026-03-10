ae773eebe6758d1ddfb18de3a7d483c70bfbaf97
HO Wing Tung <25223887@life.hkbu.edu.hk>
2026-03-10 13:29:35 +0800

    Call self introduction from Main

---
+++ src/main/kotlin/howingtung/SelfIntro.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.howingtung
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Ho Wing Tung.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
