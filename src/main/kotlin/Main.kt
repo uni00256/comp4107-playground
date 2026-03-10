ce8feb307e26febcf371fecdbbd261fc55e4635a
HO Wing Tung <25223887@life.hkbu.edu.hk>
2026-03-10 13:31:01 +0800

    Quick trunk-based change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,5 +5,5 @@ import hk.edu.hkbu.comp.comp4107.playground.wingtungho.SelfIntro
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    SelfIntroduction().selfIntroduction()
+    SelfIntroduction().selfIntroduction()//hello
 }
